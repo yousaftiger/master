d539244d31451f01f13609ca92fdbd60d14871f0
yousaftiger <43054672+yousaftiger@users.noreply.github.com>
2019-02-05 20:20:17 +0100

    Create you.js

---
+++ you.js
@@ -0,0 +1,5 @@
+<script>
+<!--
+document.write(unescape("%3Cdiv%20class%3D%27menu-center%27%3E%0A%3Cspan%3ECrafted%20With%20%3Ci%20class%3D%27fa%20fa-heart%20text-danger%27/%3E%20by%20%3Ca%20href%3D%27https%3A//youseftiger.blogspot.com/%27%20rel%3D%27nofollow%27%20title%3D%27Yousaf%20Tiger%27%3E%20Yousaf%20Tiger%3C/a%3E%3C/span%3E%0A%3C/div%3E"));
+//-->
+</script>
